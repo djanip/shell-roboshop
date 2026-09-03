ce4dacf0cf06c99ba137f584159b35164b84660d
divya <divyajanipalli.com>
2026-09-03 12:38:43 +0530

    variables

--- mongodb.sh
+++ mongodb.sh
@@ -11,14 +11,14 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
-echo "Script started ecectued at: $(date)" | tee -a $LOG_FILE
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args}
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
@@ -36,5 +36,5 @@ VALIDATE $? "Installing MongoDB"
 systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod
-VALIDATE $? "Start MongoDB"
+systemctl start mongod 
+VALIDATE $? "Enable MongoDB"
