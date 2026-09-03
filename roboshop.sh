e8993989e66fcca5e75cae3f035431e46879307f
divya <divyajanipalli.com>
2026-09-03 14:36:58 +0530

    variables

--- roboshop.sh
+++ roboshop.sh
@@ -2,17 +2,39 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0053b7a82338dec13" # replace with your SG ID
- 
+ZONE_ID="Z007473832N0S6UL3Z03Z" # replace with your ID
+DOMAIN_NAME="divyajanipalli.fun"
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0053b7a82338dec13 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-05310116ae91c29df --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.divyajanipalli.fun
     else
-        IP=$(aws ec2 describe-instances --instance-ids i-05310116ae91c29df --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $Instance_iD --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # divyajanipalli.fun
     fi
 
     echo "$instance: $IP"
+    
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"                : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done    
