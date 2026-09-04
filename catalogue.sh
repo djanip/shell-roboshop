2cbc506f03120ec6a0ec96a6558faab8fdea2422
divya <divyajanipalli.com>
2026-09-04 17:56:35 +0530

    variables

--- catalogue.sh
+++ catalogue.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.divyajanipalli.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
@@ -64,3 +65,4 @@ mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "Load catalogue products"
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
+  
