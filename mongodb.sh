bbe19feb9fe587d4c98953b8f2799b6fefbc24f2
divya <divyajanipalli.com>
2026-09-08 16:16:14 +0530

    variables

--- mongodb.sh
+++ mongodb.sh
@@ -36,7 +36,7 @@ VALIDATE $? "Installing MongoDB"
 systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod 
+systemctl start mongod
 VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
